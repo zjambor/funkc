6923ce6c4152b511ef94ab00b8bc24bd54473559
zjambor <jambor.zoltan1@gmail.com>
2019-11-13 21:33:54 +0100

    ok

--- funcprog2019-2/Hanoitornyai.hs
+++ funcprog2019-2/Hanoitornyai.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances, FlexibleContexts #-}
 module Hanoitornyai where
 
 import Control.Monad.Writer
@@ -44,21 +44,27 @@ move (largest:rest) src dst tmp = do
   move rest tmp dst src     -- move the tmp ones back onto the largest one on dst using src as temp -}
 
 moveab :: Problem -> Problem
+moveab ([], y, z) = ([], y, z)
 moveab ((x:xs), y, z) = (xs, (x:y), z)
 
 moveac :: Problem -> Problem
+moveac ([], y, z) = ([], y, z)
 moveac ((x:xs), y, z) = (xs, y, (x:z))
 
 movebc :: Problem -> Problem
+movebc (x, [], z) = (x, [], z)
 movebc (x, (y:ys), z) = (x, ys, (y:z))
 
 moveba :: Problem -> Problem
+moveba (x, [], z) = (x, [], z)
 moveba (x, (y:ys), z) = ((y:x), ys, z)
 
 moveca :: Problem -> Problem
+moveca (x, y, []) = (x, y, [])
 moveca (x, y, (z:zs)) = ((z:x), y, zs)
 
 movecb :: Problem -> Problem
+movecb (x, y, []) = (x, y, [])
 movecb (x, y, (z:zs)) = (x, (z:y), zs)
 
 move :: RodID -> RodID -> Problem -> Problem
@@ -77,5 +83,32 @@ move a b (x,y,z) =
 executeMove :: Move -> Problem -> Problem
 executeMove (a,b) (x,y,z) = move a b (x,y,z)
 
+-- executeMove (A,C) (initial 5)
 executeMoves :: [Move] -> Problem -> Problem
-executeMoves (x:xs) (x,y,z) = executeMove x (x,y,z)
+--executeMoves [] p = p
+executeMoves (x:xs) p = do
+    let c = executeMove x p
+    return c  -- (executeMoves xs c)    -- let c' =
+    --return c'
+
+--executeMoves [(A,C),(C,B)] (initial 5)
+
+applyMany :: [a -> b] -> a -> [b]
+applyMany fs a = map (\f -> f a) fs
+
+composeAll :: [a -> a] -> a -> a
+composeAll = foldr (.) id
+--composeAll [f, g, h] x = f (g (h x))
+
+paratlan = composeAll [(`mod` 2),(`mod` 3)] 6 == 1
+--composeAll [(`mod` 2),(`mod` 3)] 6
+-- composeAll [(`mod` 5),(`mod` 6),(+ 4),(* 2)] 11
+fy x = foldr (.) id [(+2), (*7)] x
+
+igaz_e = [(((+) <$> Just 3 <*> Just 3) == (Just (+3) <*> Just 3)), (((+) <$> Just 3 <*> Just 3) == (\x -> Just (x+3)) 3 )]
+
+-- Maybe as Monad:
+yep = ( Just 3 >>= \x -> return (x+3) ) == ( (\x -> Just (x+3)) 3 )
+
+main = --executeMove (A,C) (initial 5) --
+    executeMove <$> [(A,C),(C,B)] <*> [(initial 5)]

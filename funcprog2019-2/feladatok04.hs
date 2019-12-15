13915f4016daffbcca91a3ce092530d58b374cb0
zjambor <jambor.zoltan1@gmail.com>
2019-12-15 20:33:17 +0100

    ok

--- funcprog2019-2/feladatok04.hs
+++ funcprog2019-2/feladatok04.hs
@@ -34,12 +34,12 @@ data Tree2 a = Node2 a [Tree2 a] deriving Show
 --    függvény valamely elemre Nothing-ot ad, akkor Nothing legyen
 --    a végeredmény, egyébként Just <szűrt lista>
 
-filterMaybe :: (a -> Maybe Bool) -> [a] -> Maybe [a]
-filterMaybe f [] = Just []
-filterMaybe f (a:as) = case f a of
+filterMaybe' :: (a -> Maybe Bool) -> [a] -> Maybe [a]
+filterMaybe' f [] = Just []
+filterMaybe' f (a:as) = case f a of
     Nothing -> Nothing
-    Just True -> (a:) <$> (filterMaybe f as)
-    Just False -> filterMaybe f as
+    Just True -> (a:) <$> (filterMaybe' f as)
+    Just False -> filterMaybe' f as
 --
 
 fg :: Int -> Maybe Bool
@@ -48,6 +48,11 @@ fg a
     | a == 1 = Nothing
     | otherwise = Just False
 --
+fg2 :: Int -> Maybe Int
+fg2 a 
+    | a>1 = Just a
+    | a == 1 = Nothing
+    | otherwise = Just 0
 
 -- 3. Alkalmazz egy (a -> Maybe b) függvény egy Tree1 minden levelére,
 --    ha bármelyik alkalmazás Nothing-ot ad, legyen az eredmény Nothing!
@@ -61,3 +66,38 @@ mapMaybeTree f (Node1 l r) =
         Just r -> Just (Node1 l r)
 --
 -- mapMaybeTree fg (Node1 (Leaf1 1) (Leaf1 3))
+
+--  Írd meg a következő függvényt. A függvény úgy működik,
+--  mint a "filter", viszont ha a kapott (a -> Maybe Bool)
+--  függvény valamely elemre Nothing-ot ad, akkor Nothing legyen
+--  a végeredmény, egyébként Just <szűrt lista>
+
+-- Használd a Maybe monádot a megoldáshoz!
+filterMaybe :: (a -> Maybe Bool) -> [a] -> Maybe [a]
+filterMaybe f [] = pure []
+filterMaybe f (a:as) = do
+    x <- f a
+    (if x then (a:) else id) <$> filterMaybe f as
+-- filterMaybe fg [0,2,3,4,5]
+
+-- Alkalmazz egy (a -> Maybe b) függvény egy Tree1 minden levelére,
+-- ha bármelyik alkalmazás Nothing-ot ad, legyen az eredmény Nothing!
+-- Használd a Maybe monádot!
+mapMaybeTree2 :: (a -> Maybe b) -> Tree1 a -> Maybe (Tree1 b)
+mapMaybeTree2 f (Leaf1 a) = Leaf1 <$> f a
+mapMaybeTree2 f (Node1 l r) = do
+    l <- mapMaybeTree2 f l
+    r <- mapMaybeTree2 f r 
+    pure (Node1 l r)
+
+-- mapMaybeTree2 fg2 (Node1 (Leaf1 2) (Leaf1 3))
+
+-- Általánosítsd a filterMaybe függvényt tetszőleges monádra!
+-- Szűrd a listát a kapott (a -> m Bool) függvénnyel!
+filterM' :: Monad m => (a -> m Bool) -> [a] -> m [a]
+filterM' f [] = pure []
+filterM' f (a:as) = do
+    d <- f a
+    (if d then (a:) else id) <$> filterM' f as
+--
+-- filterM' fg [0,2,3,4,5]

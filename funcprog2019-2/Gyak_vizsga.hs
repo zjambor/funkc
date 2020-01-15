12be0534765aeb83a0975a588af3f12ce5a56072
zjambor <jambor.zoltan1@gmail.com>
2020-01-16 00:26:28 +0100

    ok

--- funcprog2019-2/Gyak_vizsga.hs
+++ funcprog2019-2/Gyak_vizsga.hs
@@ -32,8 +32,7 @@ ex1 :: Tree Int Int
 ex1 = Node 1 (Leaf 10) (Node 2 (Leaf 20) (Leaf 30))
 
 tree =
-  Node
-    1
+  Node 1
     (Node 2 (Node 4 (Node 7 (Leaf 20) (Leaf 20) ) (Leaf 20)) (Node 5 (Leaf 20) (Leaf 20)))
     (Node 3 (Node 6 (Node 8 (Leaf 20) (Leaf 20)) (Node 9 (Leaf 20) (Leaf 20))) (Leaf 20))
 
@@ -81,10 +80,10 @@ f x = if even x then Just (x `div` 2) else Nothing
 --Branch (Just 1) [Branch (Just 2) [Branch (Just 6) []],Branch (Just 3) [],Branch (Just 4) [Branch (Just 7) []]]
 -- traverse f ex3
 -- Just (Branch 1 [Branch 2 [Branch 6 []],Branch 3 [],Branch 4 [Branch 7 []]])
-main = traverse f ex3
+--main = traverse f ex3
 
 countElems :: RoseTree a -> Int
-countElems (Branch a []) = 1
+countElems (Branch _ []) = 1
 countElems (Branch a (x:xs)) = (countElems x) + (countElems (Branch a xs))
 --countElems ex2
 
@@ -107,8 +106,78 @@ numberElems t = evalState (traverse go t) 0 where
 
 --Segítség: Használjuk State monádot és a forM vagy mapM függvényeket!
 
---Írjunk egy függvényt, ami minden levélbe a levél fölötti Node-okban tárolt Int-ek összegét teszi. Ha nincs egy levél fölött Node, az összeg 0. (2 pont)
-helper :: Tree Int a -> State Int (Tree Int Int)
+{- helper' :: Tree a b -> State Int (Tree (a, Int) (b, Int))
+helper' (Leaf b) = do
+    n <- get
+    put (n + 1)
+    pure $ Leaf (b, n)
+helper' (Node a t1 t2) = do    
+    n <- get
+    put (n + 1)
+    t1' <- helper' t1
+    t2' <- helper' t2
+    pure (Node (a, n) t1' t2') -}
+
+-- bitraverse :: Applicative f => (a -> f a') -> (b -> f b') -> Tree a b -> f (Tree a' b')
+-- bitraverse fa fb (Leaf b) = Leaf <$> fb b
+-- bitraverse fa fb (Node a t1 t2) = Node <$> (fa a) <*> bitraverse fa fb t1 <*> bitraverse fa fb t2 
+
+{- bitraverse :: Applicative f => (a -> f a') -> (b -> f b') -> Tree a b -> f (Tree a' b')
+bitraverse f g = go where
+  go (Leaf b) = Leaf <$> g b
+  go (Node a l r) = Node <$> f a <*> go l <*> go r -}
+
+numberElems' :: Tree a b -> Tree (a, Int) (b, Int)
+--numberElems' t = evalState (helper' t) 0
+numberElems' t = evalState (bitraverse go go t) 0 where
+  go :: a -> State Int (a, Int)
+  go a = do 
+      n <- get
+      put (n + 1) 
+      pure (a, n)
+
+-- Definiáljuk azt a függvényt, amely biztonságosan beleindexel egy listába! (1 pont)
+safeIndex :: [a] -> Int -> Maybe a
+safeIndex [] _ = Nothing
+safeIndex xs a
+    | a < 0 = Nothing
+    | a >= length xs = Nothing
+    | otherwise = Just (xs !! a)
+
+ll :: [Int]
+ll = [1,2,3,4,5,6]
+main = [safeIndex ll 0 == Just 1,
+        safeIndex ll 7 == Nothing,
+        safeIndex ll (-1) == Nothing,
+        transformWithList ll (Branch 2 []) == Just (Branch 3 [])
+         ]
+
+roseList = [ex2, ex3]
+-- Definiáljuk azt a függvényt, amely egy RoseTree-ben lévő indexet lecserél egy adott lista annyiadik elemére! Az indexelés nem feltétlenül helyes, ezért Maybe-vel térjünk vissza! Ha akár egyszer is invalid lenne az index, akkor Nothing-gal térjünk vissza! (2 pont)
+transformWithList :: [Int] -> RoseTree Int -> Maybe (RoseTree Int)
+transformWithList xs (Branch a ts)
+    | safeIndex xs a == Nothing = Nothing
+    | otherwise = Just (Branch (xs !! a) ts)
+
+-- Segítség: Használjuk az előzőleg definiált safeIndex függvényt!
+
+----------------------------------------------------------------------
+-- Írjunk egy függvényt, ami az fmap-hoz hasonlóan működik, viszont mindkét típusparaméter felett map-el. (1 pont)
+bimap :: (a -> a') -> (b -> b') -> Tree a b -> Tree a' b'
+bimap f g = go where
+    go (Leaf b) = Leaf (g b)
+    go (Node a l r) = Node (f a) (go l) (go r)
+
+-- Írjunk egy függvényt, ami a traverse-hez hasonlóan működik, viszont mindkét típusparamétert bejárja. (1 pont)
+bitraverse :: Applicative f => (a -> f a') -> (b -> f b') -> Tree a b -> f (Tree a' b')
+bitraverse f g = go where
+    go (Leaf a) = Leaf <$> g a
+    go (Node a t1 t2) = Node <$> f a <*> go t1 <*> go t2 
+
+-- Írjunk egy függvényt, ami minden levélbe a levél fölötti Node-okban tárolt Int-ek összegét teszi. Ha nincs egy levél fölött Node, az összeg 0. (2 pont)
+-- annotateSums :: Tree Int a -> Tree Int Int
+
+{- helper :: Tree Int a -> State Int (Tree Int Int)
 helper (Leaf _) = do
     n <- get
     pure $ Leaf n
@@ -119,47 +188,23 @@ helper (Node n t1 t2) = do
     t1' <- helper t1
     t2' <- helper t2
     put m
-    pure $ Node n t1' t2'
+    pure $ Node n t1' t2' -}
 
 annotateSums :: Tree Int a -> Tree Int Int
-annotateSums t = evalState (helper t) 0
+annotateSums = go 0 where
+    go m (Leaf a) = Leaf m
+    go m (Node a l r) = Node a (go (a + m) l) (go (a + m) r)
 
-helper' :: Tree a b -> State Int (Tree (a, Int) (b, Int))
-helper' (Leaf b) = do
-    n <- get
-    put (n + 1)
-    pure $ Leaf (b, n)
-helper' (Node a t1 t2) = do    
-    n <- get
-    put (n + 1)
-    t1' <- helper' t1
-    t2' <- helper' t2
-    pure (Node (a, n) t1' t2')
+-- Példák a működésre:
+-- annotateSums ex1 == Node 1 (Leaf 1) (Node 2 (Leaf 3) (Leaf 3))
+-- annotateSums (Leaf 10) == Leaf 0
 
--- bitraverse :: Applicative f => (a -> f a') -> (b -> f b') -> Tree a b -> f (Tree a' b')
--- bitraverse fa fb (Leaf b) = Leaf <$> fb b
--- bitraverse fa fb (Node a t1 t2) = Node <$> (fa a) <*> bitraverse fa fb t1 <*> bitraverse fa fb t2 
 
-bitraverse :: Applicative f => (a -> f a') -> (b -> f b') -> Tree a b -> f (Tree a' b')
-bitraverse f g = go where
-  go (Leaf b) = Leaf <$> g b
-  go (Node a l r) = Node <$> f a <*> go l <*> go r
+-- Írjunk egy függvényt, ami minden Node-ba a Node alatti levelekben levő Int-ek összegét teszi. (2 pont)
+-- annotateSums' :: Tree a Int -> Tree Int Int
 
-numberElems' :: Tree a b -> Tree (a, Int) (b, Int)
-numberElems' t = evalState (helper' t) 0
--- numberElems' t = evalState (bitraverse go go t) 0 where
---   go :: a -> State Int (a, Int)
---   go a = do 
---       n <- get
---       put (n + 1) 
---       pure (a, n)
 
--- Definiáljuk azt a függvényt, amely biztonságosan beleindexel egy listába! (1 pont)
-safeIndex :: [a] -> Int -> Maybe a
-safeIndex [] a = Nothing
-safeIndex (x:xs) a = Just (x : (safeIndex xs a))
-
--- Definiáljuk azt a függvényt, amely egy RoseTree-ben lévő indexet lecserél egy adott lista annyiadik elemére! Az indexelés nem feltétlenül helyes, ezért Maybe-vel térjünk vissza! Ha akár egyszer is invalid lenne az index, akkor Nothing-gal térjünk vissza! (2 pont)
--- transformWithList :: [a] -> RoseTree Int -> Maybe (RoseTree a)
--- Segítség: Használjuk az előzőleg definiált safeIndex függvényt!
+-- Példák a működésre:
+-- annotateSums' ex1 == Node 60 (Leaf 10) (Node 50 (Leaf 20) (Leaf 30))
+-- annotateSums' (Leaf 0) == Leaf 0
 
